78b528bb2535741797b95ae5ed63af8f550c725d
someone2639 <someone2639@gmail.com>
2025-12-17 23:50:01 -0500

    t3d -> c

--- src/tiny3d/t3d.zig
+++ src/tiny3d/t3d.zig
@@ -1,5 +1,5 @@
 // Basically so that we NEVER have to call extern c code outside of here
-const t3d = @cImport({
+const c = @cImport({
     @cInclude("t3d/t3d.h");
     @cInclude("t3d/t3dmath.h");
     @cInclude("t3d/t3dmodel.h");
@@ -7,8 +7,9 @@ const t3d = @cImport({
     @cInclude("t3d/t3danim.h");
 });
 
-fn T3DVec3(xyz: [3]f32) t3d.T3DVec3 {
-    var ret: t3d.T3DVec3 = undefined;
+// Constructs a T3DVec3 from a [3]f32 so that we can talk to C
+fn T3DVec3(xyz: [3]f32) c.T3DVec3 {
+    var ret: c.T3DVec3 = undefined;
     ret.v = .{xyz[0], xyz[1], xyz[2]};
 
     return ret;
@@ -33,26 +34,26 @@ pub const Vec3 = struct {
 };
 
 pub const Viewport = struct {
-    vp: t3d.T3DViewport,
+    vp: c.T3DViewport,
 
     pub fn create(num_framebuffers: u16) Viewport {
         return .{
-            .vp = t3d.t3d_viewport_create_buffered(num_framebuffers),
+            .vp = c.t3d_viewport_create_buffered(num_framebuffers),
         };
     }
 
     pub fn set_projection(self: *Viewport, fov: f32, near: f32, far: f32) void {
-        t3d.t3d_viewport_set_projection(&self.vp, fov, near, far);
+        c.t3d_viewport_set_projection(&self.vp, fov, near, far);
     }
 
     pub fn look_at(self: *Viewport, eye: [3]f32, look: [3]f32, up: [3]f32) void {
         var eye_vec = T3DVec3(eye);
         var look_vec = T3DVec3(look);
         var up_vec = T3DVec3(up);
-        t3d.t3d_viewport_look_at(&self.vp, &eye_vec, &look_vec, &up_vec);
+        c.t3d_viewport_look_at(&self.vp, &eye_vec, &look_vec, &up_vec);
     }
 
     pub fn attach(self: *Viewport) void {
-        t3d.t3d_viewport_attach(&self.vp);
+        c.t3d_viewport_attach(&self.vp);
     }
 };
