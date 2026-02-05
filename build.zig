d2174b6fc7e9ca63c693c5e4680bc1f790459bcc
someone2639 <someone2639@gmail.com>
2026-02-04 21:57:13 -0500

    add build.zig (IT WONT WORK

---
+++ build.zig
@@ -0,0 +1,42 @@
+const std = @import("std");
+
+// Very simple build system, get a C file out of a zig file, then `make` the rest
+//  This is partly because Zig 
+
+pub fn build(b: *std.Build) void {
+    const target = b.resolveTargetQuery(
+        .{
+            .cpu_arch = .mips,
+            .os_tag = .freestanding,
+            .abi = .gnu,
+            .cpu_model = .{
+                .explicit = &std.Target.mips.cpu.mips2
+            },
+            .ofmt = .c,
+        }
+    );
+
+    const obj = b.addObject(.{
+        .name = "game",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+        }),
+    });
+
+    obj.addIncludePath(b.path("include"));
+    obj.addIncludePath(b.path("src"));
+
+    obj.addSystemIncludePath(.{
+        .cwd_relative = "/usr/mips64-elf/include",
+    });
+
+    obj.linkLibC();
+
+    const install_c = b.addInstallFile(
+        obj.getEmittedFileGeneric(),
+        "main.c",
+    );
+
+    b.getInstallStep().dependOn(&install_c.step);
+}
